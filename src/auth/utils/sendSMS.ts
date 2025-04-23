dafa5c0fa55f71d28b1be5b85e18d76a86483d3e
Bassel Manasrah <bassel.manasrah@gmail.com>
2025-04-23 11:40:18 +0300

    cooment out sendSMS

--- src/auth/utils/sendSMS.ts
+++ src/auth/utils/sendSMS.ts
@@ -1,25 +1,25 @@
 import { Vonage } from "@vonage/server-sdk";
 
-const vonage = new Vonage({
-  apiKey: "af381996",
-  apiSecret: "3mr8Vrrm9F5YxpII",
-});
+// const vonage = new Vonage({
+//   apiKey: "af381996",
+//   apiSecret: "3mr8Vrrm9F5YxpII",
+// });
 
-const from = "Vonage APIs";
-const to = "972585100114";
-const text = "A text message sent using the Vonage SMS API";
+// const from = "Vonage APIs";
+// const to = "972585100114";
+// const text = "A text message sent using the Vonage SMS API";
 
 const sendSMS = async () => {
-  await vonage.sms
-    .send({ to, from, text })
-    .then((resp) => {
-      console.log("Message sent successfully");
-      console.log(resp);
-    })
-    .catch((err) => {
-      console.log("There was an error sending the messages.");
-      console.error(err);
-    });
+  // await vonage.sms
+  //   .send({ to, from, text })
+  //   .then((resp) => {
+  //     console.log("Message sent successfully");
+  //     console.log(resp);
+  //   })
+  //   .catch((err) => {
+  //     console.log("There was an error sending the messages.");
+  //     console.error(err);
+  //   });
 };
 
 export default sendSMS;
